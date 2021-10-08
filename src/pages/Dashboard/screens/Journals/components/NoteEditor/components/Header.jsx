ed96dfcc8398c61324027472bf6699b9701dec93
Anmol Virk <theanmolvirk@gmail.com>
2021-10-09 03:12:44 +0530

    Get Style of focused text and show status in text editor header

--- src/pages/Dashboard/screens/Journals/components/NoteEditor/components/Header.jsx
+++ src/pages/Dashboard/screens/Journals/components/NoteEditor/components/Header.jsx
@@ -2,43 +2,126 @@ import React, {useState} from 'react'
 import {Bold, Italic, Underline, List, ChevronDown, ChevronRight, CornerUpLeft, CornerUpRight} from 'react-feather'
 import styles from './_header.module.sass'
 
-const TextStyleOptions = () => (
-    <ul className={styles.textStyleOptions}>
-        <li onMouseDown={()=>document.execCommand('bold')}><Bold /></li>
-        <li onMouseDown={()=>document.execCommand('italic')}><Italic /></li>
-        <li onMouseDown={()=>document.execCommand('underline')}><Underline /></li>
-    </ul>
-)
-
-const Colors = ({colors}) => (
-    <ul className={styles.colors}>
-        {colors.map((color, index)=>{
-            return <li key={index} style={{backgroundColor: color}} onMouseDown={()=>document.execCommand('foreColor', false, color)} />
-        })}
-    </ul>
-)
-
-let undoInterval
-
-const undoStart = () => {
-    undoInterval = setInterval(()=>{document.execCommand('undo')}, 10)
-}
-
-let redoInterval
-
-const redoStart = () => {
-    redoInterval = setInterval(()=>{document.execCommand('redo')}, 10)
-}
-
-const TimeControl = () => (
-    <ul className={styles.textStyleOptions}>
-        <li onMouseDown={undoStart} onMouseUp={()=>clearInterval(undoInterval)}><CornerUpLeft /></li>
-        <li onMouseDown={redoStart} onMouseUp={()=>clearInterval(redoInterval)}><CornerUpRight /></li>
-    </ul>
-)
-
 const Header = ({colors, bold}) => {
 
+    document.addEventListener('mouseup', function() {
+        if(document.queryCommandState("bold")){
+            setIsBold(true)
+        }else{
+            setIsBold(false)
+        }
+        if(document.queryCommandState("italic")){
+            setIsItalic(true)
+        }else{
+            setIsItalic(false)
+        }
+        if(document.queryCommandState("underline")){
+            setIsUnderline(true)
+        }else{
+            setIsUnderline(false)
+        }
+        if(document.queryCommandState("insertUnorderedList")){
+            setIsList(true)
+        }else{
+            setIsList(false)
+        }
+        switch (document.queryCommandValue('formatBlock')) {
+            case 'h1':
+                setCurrentTextSize('Heading')
+            break
+            case 'h3':
+                setCurrentTextSize('Sub Heading')
+            break
+            case 'div':
+                setCurrentTextSize('Normal')
+            break
+            case 'p':
+                setCurrentTextSize('Normal')
+            break
+            default:
+                break;
+        }
+    }, false)
+
+    document.addEventListener('keyup', function() {
+        if(document.queryCommandState("bold")){
+            setIsBold(true)
+        }else{
+            setIsBold(false)
+        }
+        if(document.queryCommandState("italic")){
+            setIsItalic(true)
+        }else{
+            setIsItalic(false)
+        }
+        if(document.queryCommandState("underline")){
+            setIsUnderline(true)
+        }else{
+            setIsUnderline(false)
+        }
+        if(document.queryCommandState("insertUnorderedList")){
+            setIsList(true)
+        }else{
+            setIsList(false)
+        }
+        switch (document.queryCommandValue('formatBlock')) {
+            case 'h1':
+                setCurrentTextSize('Heading')
+            break
+            case 'h3':
+                setCurrentTextSize('Sub Heading')
+            break
+            case 'div':
+                setCurrentTextSize('Normal')
+            break
+            case 'p':
+                setCurrentTextSize('Normal')
+            break
+            default:
+                break;
+        }
+    }, false)
+
+    const [isBold, setIsBold] = useState(false)
+    const [isItalic, setIsItalic] = useState(false)
+    const [isUnderline, setIsUnderline] = useState(false)
+    const [isList, setIsList] = useState(false)
+
+
+    const TextStyleOptions = () => (
+        <ul className={styles.textStyleOptions}>
+            <li onMouseDown={()=>document.execCommand('bold')} className={isBold ?  styles.activeBtn : null}><Bold /></li>
+            <li onMouseDown={()=>document.execCommand('italic')} className={isItalic ?  styles.activeBtn : null}><Italic /></li>
+            <li onMouseDown={()=>document.execCommand('underline')} className={isUnderline ?  styles.activeBtn : null}><Underline /></li>
+        </ul>
+    )
+    
+    const Colors = ({colors}) => (
+        <ul className={styles.colors}>
+            {colors.map((color, index)=>{
+                return <li key={index} style={{backgroundColor: color}} onMouseDown={()=>document.execCommand('foreColor', false, color)} />
+            })}
+        </ul>
+    )
+    
+    let undoInterval
+    
+    const undoStart = () => {
+        undoInterval = setInterval(()=>{document.execCommand('undo')}, 10)
+    }
+    
+    let redoInterval
+    
+    const redoStart = () => {
+        redoInterval = setInterval(()=>{document.execCommand('redo')}, 10)
+    }
+    
+    const TimeControl = () => (
+        <ul className={styles.textStyleOptions}>
+            <li onMouseDown={undoStart} onMouseUp={()=>clearInterval(undoInterval)}><CornerUpLeft /></li>
+            <li onMouseDown={redoStart} onMouseUp={()=>clearInterval(redoInterval)}><CornerUpRight /></li>
+        </ul>
+    )
     
     const [textDropDown, setTextDropDown] = useState(false)
 
@@ -87,7 +170,7 @@ const Header = ({colors, bold}) => {
                 <div className={styles.textOptions}>
                     <TextSizeDropDown />
                     <TextStyleOptions bold={bold} />
-                    <List className={styles.list} onMouseDown={()=>document.execCommand('insertUnorderedList')} />
+                    <List onMouseDown={()=>document.execCommand('insertUnorderedList')}  className={isList ?  `${styles.activeBtn} ${styles.list}` : styles.list} />
                     <TimeControl />
                 </div>
                 <Colors colors={colors} />
