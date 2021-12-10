ccbdd31a1ce9d8639d1abff9b5f66c7cec1a5724
Anmol Virk <theanmolvirk@gmail.com>
2021-12-10 20:13:38 +0530

    Schedule Redirect

--- src/pages/Dashboard/screens/Schedule/index.jsx
+++ src/pages/Dashboard/screens/Schedule/index.jsx
@@ -4,11 +4,15 @@ import ScheduleSection from './components/ScheduleSection'
 import MainCalendar from './components/Calendar'
 import { useRecoilState } from 'recoil'
 import allCalendarEventsAtom from './recoil-atoms/allCalendarEventsAtom'
+import { Redirect } from 'react-router'
+import allRoutesAtom from '../Journals/recoil-atoms/allRoutesAtom'
 
 const Schedule = () => {
     const [allCalendarEvents] = useRecoilState(allCalendarEventsAtom)
+    const [allRoutes] = useRecoilState(allRoutesAtom)
     return (
         <div style={{display: 'flex'}}>
+            <Redirect to={allRoutes&&allRoutes['scheduleSection']?`/schedule/${allRoutes['scheduleSection']}/${allRoutes['scheduleSection']==='habits'?allRoutes['habit']?allRoutes['habit']:'':allRoutes['scheduleSection']==='tasks'?allRoutes['project']?allRoutes['project']:'':''}`:'/schedule/habits'} />
             <SideBar />
             <ScheduleSection />
             {allCalendarEvents.length>0?<MainCalendar />:null}
