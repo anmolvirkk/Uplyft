cba6d8f6210239204b01407c662fe03b583b93d7
Anmol Virk <theanmolvirk@gmail.com>
2022-01-04 17:12:06 +0530

    Features

--- src/screens/Subsidiary/LandingPage/components/Features/index.jsx
+++ src/screens/Subsidiary/LandingPage/components/Features/index.jsx
@@ -1,23 +1,77 @@
-import React from 'react'
-import { Edit2 } from 'react-feather'
+import React, {useState} from 'react'
+import { Book, Edit2, Edit, Grid, Navigation, Sliders, Moon, CornerDownRight, Calendar, Folder, RefreshCw } from 'react-feather'
 import styles from './_features.module.sass'
 import company from '../../../../../company'
 
 const features = {
     journals: [
         {
-            title: '',
+            title: 'Customizable Journals',
             description: '',
-            icon: <Edit2 />,
-            img: 'journals.png'
+            icon: <Sliders />,
+            img: 'customJournals.png'
+        },
+        {
+            title: 'Multiple Journals',
+            description: '',
+            icon: <Book />,
+            img: 'multipleJournals.png'
+        },
+        {
+            title: 'Multiple Notes',
+            description: '',
+            icon: <Grid />,
+            img: 'multipleNotes.png'
+        },
+        {
+            title: 'Prompts to get you started',
+            description: '',
+            icon: <Navigation />,
+            img: 'journalPrompts.png'
+        },
+        {
+            title: 'Minimalistic Note Editor',
+            description: '',
+            icon: <Edit />,
+            img: 'journalEditor.png'
+        },
+        {
+            title: 'Dark Mode',
+            description: '',
+            icon: <Moon />,
+            img: 'darkModeJournals.png'
         }
     ],
     schedule: [
         {
-            title: '',
+            title: 'Track habits',
             description: '',
-            icon: <Edit2 />,
-            img: ''
+            icon: <RefreshCw />,
+            img: 'trackHabits.png'
+        },
+        {
+            title: 'Track projects',
+            description: '',
+            icon: <Folder />,
+            img: 'trackProjects.png'
+        },
+        {
+            title: 'Track events',
+            description: '',
+            icon: <Calendar />,
+            img: 'trackEvents.png'
+        },
+        {
+            title: 'Multiple Subtasks',
+            description: '',
+            icon: <CornerDownRight />,
+            img: 'multipleSubtasks.png'
+        },
+        {
+            title: 'Dark Mode',
+            description: '',
+            icon: <Moon />,
+            img: 'darkModeSchedule.png'
         }
     ],
     notes: [
@@ -47,30 +101,46 @@ const features = {
 }
 
 const Features = () => {
+    
+    const [currentFeature, setCurrentFeature] = useState({icon: 'journals', feature: 0})
+
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.icons}>
-                <img src='/logos/journals.png' alt={company.journals} />
-                <img src='/logos/schedule.png' alt={company.schedule} />
-                <img src='/logos/notes.png' alt={company.notes} />
-                <img src='/logos/finances.png' alt={company.finances} />
-                <img src='/logos/fitness.png' alt={company.fitness} />
-            </div>
-            <div className={styles.features}>
-                {features.journals.map((item)=>{
-                    return (
-                        <div className={styles.feature}>
-                            <div className={styles.title}>
-                                {item.icon}
-                                <h3>{item.title}</h3>
+        <div className={styles.featureSection}>
+            <h2>The Features</h2>
+            <div className={styles.wrapper}>
+                <div className={styles.icons}>
+                    <div className={currentFeature.icon === 'journals'?styles.activeIcon:null}>
+                        <img src='/logos/journals.png' alt={company.journals} onMouseDown={()=>setCurrentFeature({feature: 0, icon: 'journals'})} />
+                    </div>
+                    <div className={currentFeature.icon === 'schedule'?styles.activeIcon:null}>
+                        <img src='/logos/schedule.png' alt={company.schedule} onMouseDown={()=>setCurrentFeature({feature: 0, icon: 'schedule'})} />
+                    </div>
+                    <div className={currentFeature.icon === 'notes'?styles.activeIcon:null}>
+                        <img src='/logos/notes.png' alt={company.notes} onMouseDown={()=>setCurrentFeature({feature: 0, icon: 'notes'})} />
+                    </div>
+                    <div className={currentFeature.icon === 'finances'?styles.activeIcon:null}>
+                        <img src='/logos/finances.png' alt={company.finances} onMouseDown={()=>setCurrentFeature({feature: 0, icon: 'finances'})} />
+                    </div>
+                    <div className={currentFeature.icon === 'fitness'?styles.activeIcon:null}>
+                        <img src='/logos/fitness.png' alt={company.fitness} onMouseDown={()=>setCurrentFeature({feature: 0, icon: 'fitness'})} />
+                    </div>
+                </div>
+                <div className={styles.features}>
+                    {features[currentFeature.icon].map((item, i)=>{
+                        return (
+                            <div key={i} onMouseDown={()=>setCurrentFeature({...currentFeature, feature: i})} className={currentFeature.feature===i?`${styles.activeFeature} ${styles.feature}`:styles.feature}>
+                                <div className={styles.title}>
+                                    {item.icon}
+                                    <h3>{item.title}</h3>
+                                </div>
+                                <p className={styles.description}>{item.description}</p>
                             </div>
-                            <p className={styles.description}>{item.description}</p>
-                        </div>
-                    )
-                })}
-            </div>
-            <div className={styles.content}>
-                <img src={`/screens/${features.journals[0].img}`} alt='' />
+                        )
+                    })}
+                </div>
+                <div className={styles.content}>
+                    <img src={`/screens/${features[currentFeature.icon][currentFeature.feature].img}`} alt='' />
+                </div>
             </div>
         </div>
     )
