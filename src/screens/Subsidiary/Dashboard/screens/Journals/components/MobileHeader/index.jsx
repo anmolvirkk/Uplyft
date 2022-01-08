6b2362f8d570889ea19fad6d8880f6c29f50ccf9
Anmol Virk <theanmolvirk@gmail.com>
2022-01-08 17:00:12 +0530

    Add journal mobile version

--- src/screens/Subsidiary/Dashboard/screens/Journals/components/MobileHeader/index.jsx
+++ src/screens/Subsidiary/Dashboard/screens/Journals/components/MobileHeader/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from './_mobileHeader.module.sass'
 import {MoreVertical, Plus} from 'react-feather'
+import modalConfigAtom from '../../recoil-atoms/modalConfigAtom'
+import { useSetRecoilState } from 'recoil'
 
 const MobileHeader = () => {
+    const setModalConfig = useSetRecoilState(modalConfigAtom)
     return (
         <div className={styles.header}>
-            <p>Spirit</p>
+            <p>Journals</p>
             <div className={styles.options}>
-                <Plus />
+                <Plus onMouseDown={()=>setModalConfig({type: 'addjournal'})} />
                 <MoreVertical />
             </div>
         </div>
