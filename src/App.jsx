a69d4032484c2570987f741126df3cf0a2de9edf
Anmol Virk <theanmolvirk@gmail.com>
2022-01-18 15:52:11 +0530

    changing darkmode class location to top level (html tag)

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,25 @@ import Auth from './screens/Subsidiary/Auth'
 const App = () => {
     
     
-    document.getElementsByTagName('html')[0].style.maxHeight = window.innerHeight+'px'
-    document.getElementsByTagName('body')[0].style.maxHeight = window.innerHeight+'px'
+    document.getElementsByTagName('html')[0].style.height = window.innerHeight+'px'
+    document.getElementsByTagName('body')[0].style.height = window.innerHeight+'px'
 
     const [darkMode] = useRecoilState(darkModeAtom)
 
     useEffect(()=>{
-        document.getElementsByTagName('html')[0].style.backgroundColor = darkMode?"rgb(11,11,11)":"rgb(255, 255, 255)"
-        document.getElementsByTagName('body')[0].style.backgroundColor = darkMode?"rgb(11,11,11)":"rgb(255, 255, 255)"
+        document.getElementsByTagName('html')[0].className = darkMode?'dark':'light'
     }, [darkMode])
 
     return (
-        <div className={(darkMode?'dark':'light')}>
-            <Router>
-                <Switch>
-                    <Route exact path="/"><Redirect to={`/${company.subsidiary}`} /></Route>
-                    <Route exact path={`/${company.subsidiary}/signup`}><Auth type='signup' /></Route>
-                    <Route exact path={`/${company.subsidiary}/login`}><Auth type='login' /></Route>
-                    <Route exact path={`/${company.subsidiary}`}><LandingPage /></Route>
-                    <Route path={`/${company.subsidiary}/dashboard`}><Dashboard /></Route>
-                </Switch>
-            </Router>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/"><Redirect to={`/${company.subsidiary}`} /></Route>
+                <Route exact path={`/${company.subsidiary}/signup`}><Auth type='signup' /></Route>
+                <Route exact path={`/${company.subsidiary}/login`}><Auth type='login' /></Route>
+                <Route exact path={`/${company.subsidiary}`}><LandingPage /></Route>
+                <Route path={`/${company.subsidiary}/dashboard`}><Dashboard /></Route>
+            </Switch>
+        </Router>
     )
 }
 
