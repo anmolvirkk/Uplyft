c3f5502120bef9ea1f4589155dac79ccf58a1595
Anmol Virk <theanmolvirk@gmail.com>
2022-01-19 15:40:42 +0530

    new refocus

--- src/screens/Subsidiary/Dashboard/screens/Journals/components/MainSection/components/NoteEditor/index.jsx
+++ src/screens/Subsidiary/Dashboard/screens/Journals/components/MainSection/components/NoteEditor/index.jsx
@@ -30,13 +30,8 @@ const TextEditor = ({prompt, value, setEditorData, setNote, id, name, category, 
     }
   }, [isMobile])
 
-  let refocusTimeout
-
   const refocus = (e) => {
-    clearTimeout(refocusTimeout)
-    refocusTimeout = setTimeout(()=>{
-      document.getElementById('textEditor').scrollTo(0, (e.target.offsetTop-50-12))
-    }, 500)
+    document.getElementById('textEditor').scrollTo(0, (e.target.offsetTop-50-125))
   }
 
   return <div onMouseDown={isMobile?(e)=>refocus(e):null} id='textEditor' ref={textEditor} contentEditable data-placeholder="Start Writing..." onInput={(e)=>handleInput(e.target.innerHTML)} dangerouslySetInnerHTML={{__html: editorBody.current}} className={styles.textEditor} style={{paddingTop: allPrompts[category.replace(/ /g, "")]&&allPrompts[category.replace(/ /g, "")].length<=0 ? '2.5vh' : null}} />
