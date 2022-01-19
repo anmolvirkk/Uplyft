210e7276cea736984e4b6725629c57e4713a4fb6
Anmol Virk <theanmolvirk@gmail.com>
2022-01-19 14:38:55 +0530

    updated prompts height

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,13 @@ const App = () => {
                 document.getElementById('textEditorHeader').style.display = 'block'
                 document.getElementById('textEditor').style.height = (window.innerHeight - 60 - 40 - 12 - 24 - 39)+'px'
                 document.getElementById('textEditor').style.marginBottom = '38px'
-                document.getElementById('promptsSelector').style.height = (window.innerHeight - 60 - 40 - 40 - 24 - 3)+'px'
+                document.getElementById('promptsSelector').style.height = (window.innerHeight - 60 - 40 - 40 - 24)+'px'
             }else{
                 document.getElementById('mainSideBar').style.display = 'flex'
                 document.getElementById('textEditorHeader').style.display = 'none'
                 document.getElementById('textEditor').style.height = (window.innerHeight - 80 - 60 - 50 - 12 - 24)+'px'
                 document.getElementById('textEditor').style.marginBottom = '12px'
-                document.getElementById('promptsSelector').style.height = (window.innerHeight - 60 - 80 - 40 - 24 - 14)+'px'
+                document.getElementById('promptsSelector').style.height = (window.innerHeight - 60 - 80 - 40 - 24 - 10)+'px'
             }
         }
 
