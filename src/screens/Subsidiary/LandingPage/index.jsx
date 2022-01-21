e880ebabe87f0ad2cf2e874b46230afcbe4b64d8
Anmol Virk <theanmolvirk@gmail.com>
2022-01-21 20:13:24 +0530

    container window height

--- src/screens/Subsidiary/LandingPage/index.jsx
+++ src/screens/Subsidiary/LandingPage/index.jsx
@@ -6,10 +6,11 @@ import styles from './_main.module.sass'
 import About from './components/About'
 import Trust from './components/Trust'
 import Features from './components/Features'
+import {windowHeight} from '../Dashboard/variables/mobileHeights'
 
 const LandingPage = () => {
     return (
-        <div className={`${styles.container} light`} style={{height: window.innerHeight}}>
+        <div className={`${styles.container} light`} style={{height: windowHeight+'px'}}>
             <Header />
             <Hero />
             <About />
