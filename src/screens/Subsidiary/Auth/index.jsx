ce61dc93bc476090d8ff30b79f56b9e087c28de6
Anmol Virk <theanmolvirk@gmail.com>
2022-01-21 20:53:11 +0530

    sign up mobile scroll wrapper

--- src/screens/Subsidiary/Auth/index.jsx
+++ src/screens/Subsidiary/Auth/index.jsx
@@ -7,37 +7,38 @@ import {windowHeight} from '../Dashboard/variables/mobileHeights'
 
 const Auth = () => {
     useEffect(()=>{
-        console.log('x')
         document.getElementsByTagName('html')[0].className = 'light'
     }, [])
     return (
-        <div className={styles.auth} style={{height: windowHeight+'px'}}>
-            <div className={styles.form}>
-                <div className={styles.title}>
-                    <img src='/logos/main.png' alt={company.subsidiary} />
-                    <h1>Skyhance</h1>
-                    <p>sign up to continue to Uplyft</p>
-                </div>
-                <div className={styles.social}>
-                <GoogleLogin
-                    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-                    buttonText="Continue with Google"
-                />
-                </div>
-                <div className={styles.divide}>
-                    <hr />
-                    <p>or</p>
-                    <hr />
-                </div>
-                <div className={styles.input}>
-                    <input type='text' placeholder='Email' />
-                    <input type='password' placeholder='Password' />
-                    <input type='password' placeholder='Confirm Password' />
-                    <Link to={`/${company.subsidiary}/dashboard/${company.journals}`}>Continue</Link>
-                </div>
-                <div className={styles.signin}>
-                    <p>Already have an account?</p>
-                    <Link to={`/${company.subsidiary}/signin`}>Sign In</Link>
+        <div className={styles.wrapper} style={{height: window.innerHeight+'px'}}>
+            <div className={styles.auth} style={{height: windowHeight+'px'}}>
+                <div className={styles.form}>
+                    <div className={styles.title}>
+                        <img src='/logos/main.png' alt={company.subsidiary} />
+                        <h1>Skyhance</h1>
+                        <p>sign up to continue to Uplyft</p>
+                    </div>
+                    <div className={styles.social}>
+                    <GoogleLogin
+                        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                        buttonText="Continue with Google"
+                    />
+                    </div>
+                    <div className={styles.divide}>
+                        <hr />
+                        <p>or</p>
+                        <hr />
+                    </div>
+                    <div className={styles.input}>
+                        <input type='text' placeholder='Email' />
+                        <input type='password' placeholder='Password' />
+                        <input type='password' placeholder='Confirm Password' />
+                        <Link to={`/${company.subsidiary}/dashboard/${company.journals}`}>Continue</Link>
+                    </div>
+                    <div className={styles.signin}>
+                        <p>Already have an account?</p>
+                        <Link to={`/${company.subsidiary}/signin`}>Sign In</Link>
+                    </div>
                 </div>
             </div>
         </div>
