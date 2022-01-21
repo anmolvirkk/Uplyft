4bca8364022c9248fd43a360012c30ebac50db2d
Anmol Virk <theanmolvirk@gmail.com>
2022-01-22 03:56:42 +0530

    mobile test

--- src/screens/Subsidiary/Dashboard/screens/Journals/components/MainSection/components/NoteEditor/index.jsx
+++ src/screens/Subsidiary/Dashboard/screens/Journals/components/MainSection/components/NoteEditor/index.jsx
@@ -26,7 +26,9 @@ const TextEditor = ({prompt, value, editorData, setEditorData, setNote, id, name
 
       document.getElementById('textEditor').style.height = (window.innerHeight - 80 - 60 - 50 - 12 - 24 - 3)+'px'
       document.getElementById('textEditor').style.opacity = 1
+      alert(document.getElementById('textEditor').children.length)
       for(let i=0; i<document.getElementById('textEditor').children.length; i++){
+        alert(document.getElementById('textEditor').children[i])
         if(!document.getElementById('textEditor').children[i].onmousedown){
           document.getElementById('textEditor').children[i].onmousedown = (e) => {
             alert(e.target.offsetTop)
