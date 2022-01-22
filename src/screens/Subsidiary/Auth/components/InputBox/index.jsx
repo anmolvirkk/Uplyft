842d6aace77f6e01a3497178844f91dfdbc43221
Anmol Virk <theanmolvirk@gmail.com>
2022-01-22 23:38:14 +0530

    position static for modal

--- src/screens/Subsidiary/Auth/components/InputBox/index.jsx
+++ src/screens/Subsidiary/Auth/components/InputBox/index.jsx
@@ -1,21 +1,17 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import styles from './_inputBox.module.sass'
 import { windowHeight, isMobile } from '../../../Dashboard/variables/mobileHeights'
 
-const scrollToView = (e, wrapper, shouldFocus, focusInput) => {
+const scrollToView = (e, wrapper) => {
     let elem = document.getElementById(wrapper)
     if(elem){
         const top = elem.scrollHeight-(elem.scrollHeight-(elem.scrollTop+e.clientY))
         if(window.innerHeight < windowHeight){
             elem.scroll({top: top/2, behavior: 'smooth'})
-            shouldFocus.current = true
-            focusInput.current.focus()
         }else if(isMobile){
             setTimeout(()=>{
                 if(window.innerHeight < windowHeight){
                     elem.scroll({top: top/2, behavior: 'smooth'})
-                    shouldFocus.current = true
-                    focusInput.current.focus()
                 }
             }, 500)
         }
@@ -23,16 +19,9 @@ const scrollToView = (e, wrapper, shouldFocus, focusInput) => {
 }
 
 const InputBox = ({wrapper, name, type, marginBottom, value, onChange}) => {
-    const focusInput = useRef()
-    const shouldFocus = useRef(false)
-    useEffect(()=>{
-        if(focusInput.current && shouldFocus.current){
-            focusInput.current.focus()
-        }
-    }, [focusInput])
     return (
-    <div onMouseDown={(e)=>scrollToView(e, wrapper, shouldFocus, focusInput)} className={styles.group} style={marginBottom?{marginBottom: marginBottom+'px'}:null}>
-        <input ref={focusInput} onChange={onChange?e=>onChange(e):null} defaultValue={value?value:null} type={type} required="required"/><span className={styles.highlight}></span><span className={styles.bar}></span>
+    <div onMouseDown={(e)=>scrollToView(e, wrapper)} className={styles.group} style={marginBottom?{marginBottom: marginBottom+'px'}:null}>
+        <input onChange={onChange?e=>onChange(e):null} defaultValue={value?value:null} type={type} required="required"/><span className={styles.highlight}></span><span className={styles.bar}></span>
         <label>{name}</label>
     </div>
     )
