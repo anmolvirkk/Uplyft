96f5fc23cce5ff3fc441029384443f9dd8f1f42b
Anmol Virk <theanmolvirk@gmail.com>
2022-01-22 23:26:53 +0530

    focus input

--- src/screens/Subsidiary/Auth/components/InputBox/index.jsx
+++ src/screens/Subsidiary/Auth/components/InputBox/index.jsx
@@ -2,20 +2,23 @@ import React, { useEffect, useRef } from 'react'
 import styles from './_inputBox.module.sass'
 import { windowHeight, isMobile } from '../../../Dashboard/variables/mobileHeights'
 
-const scrollToView = (e, wrapper, shouldFocus) => {
+const scrollToView = (e, wrapper, shouldFocus, focusInput) => {
     let elem = document.getElementById(wrapper)
     if(elem){
         const top = elem.scrollHeight-(elem.scrollHeight-(elem.scrollTop+e.clientY))
         if(window.innerHeight < windowHeight){
             elem.scroll({top: top/2, behavior: 'smooth'})
+            shouldFocus.current = true
+            focusInput.current.focus()
         }else if(isMobile){
             setTimeout(()=>{
                 if(window.innerHeight < windowHeight){
                     elem.scroll({top: top/2, behavior: 'smooth'})
+                    shouldFocus.current = true
+                    focusInput.current.focus()
                 }
             }, 500)
         }
-        shouldFocus.current = true
     }
 }
 
@@ -28,7 +31,7 @@ const InputBox = ({wrapper, name, type, marginBottom, value, onChange}) => {
         }
     }, [focusInput])
     return (
-    <div onMouseDown={(e)=>scrollToView(e, wrapper, shouldFocus)} className={styles.group} style={marginBottom?{marginBottom: marginBottom+'px'}:null}>
+    <div onMouseDown={(e)=>scrollToView(e, wrapper, shouldFocus, focusInput)} className={styles.group} style={marginBottom?{marginBottom: marginBottom+'px'}:null}>
         <input ref={focusInput} onChange={onChange?e=>onChange(e):null} defaultValue={value?value:null} type={type} required="required"/><span className={styles.highlight}></span><span className={styles.bar}></span>
         <label>{name}</label>
     </div>
