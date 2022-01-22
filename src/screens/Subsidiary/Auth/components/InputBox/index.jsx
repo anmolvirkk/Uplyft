69225de2a092654782a3729b8f0062db7c9f90f6
Anmol Virk <theanmolvirk@gmail.com>
2022-01-22 23:18:21 +0530

    focus without useeffect

--- src/screens/Subsidiary/Auth/components/InputBox/index.jsx
+++ src/screens/Subsidiary/Auth/components/InputBox/index.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import styles from './_inputBox.module.sass'
 import { windowHeight, isMobile } from '../../../Dashboard/variables/mobileHeights'
 
-const scrollToView = (e, wrapper, id) => {
+const scrollToView = (e, wrapper, focusInput) => {
+    focusInput.current.focus()
     let elem = document.getElementById(wrapper)
     if(elem){
         const top = elem.scrollHeight-(elem.scrollHeight-(elem.scrollTop+e.clientY))
@@ -20,14 +21,8 @@ const scrollToView = (e, wrapper, id) => {
 
 const InputBox = ({wrapper, name, type, marginBottom, value, onChange}) => {
     const focusInput = useRef()
-    useEffect(()=>{
-        if(focusInput.current){
-            focusInput.current.focus()
-            console.log(focusInput.current)
-        }
-    }, [focusInput])
     return (
-    <div onMouseDown={(e)=>scrollToView(e, wrapper)} className={styles.group} style={marginBottom?{marginBottom: marginBottom+'px'}:null}>
+    <div onMouseDown={(e)=>scrollToView(e, wrapper, focusInput)} className={styles.group} style={marginBottom?{marginBottom: marginBottom+'px'}:null}>
         <input ref={focusInput} onChange={onChange?e=>onChange(e):null} defaultValue={value?value:null} type={type} required="required"/><span className={styles.highlight}></span><span className={styles.bar}></span>
         <label>{name}</label>
     </div>
