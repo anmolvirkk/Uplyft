1af10302ee39d9ee05ed38d9a24421667ecc4f6f
Anmol Virk <theanmolvirk@gmail.com>
2022-01-30 12:53:15 +0530

    backendless login and signup

--- src/screens/Subsidiary/Auth/components/InputBox/index.jsx
+++ src/screens/Subsidiary/Auth/components/InputBox/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './_inputBox.module.sass'
 import { windowHeight } from '../../../Dashboard/variables/mobileHeights'
 import { useRecoilState } from 'recoil'
@@ -28,13 +28,29 @@ const scrollToView = (e, wrapper, isMobile) => {
     }
 }
 
-const InputBox = ({wrapper, name, type, marginBottom, value, onChange, onBlur, icon, autoFocus, onFocus, id, autoComplete, onTouchEnd}) => {
+const InputBox = ({wrapper, name, type, marginBottom, value, onChange, onBlur, icon, autoFocus, onFocus, id, autoComplete, onTouchEnd, error, onLoad}) => {
     const [isMobile] = useRecoilState(isMobileAtom)
+    const input = useRef()
+    if(input.current){
+        if(error){
+            input.current.style.setProperty('--inputColor', 'rgb(255,51,51)')
+            input.current.style.setProperty('--initialBar', '100%')
+            input.current.style.setProperty('--initialLabelFontSize', '12px')
+            input.current.style.setProperty('--initialLabelTop', '-15px')
+            input.current.style.setProperty('--labelColor', 'rgb(255,51,51)')
+        }else{
+            input.current.style.setProperty('--inputColor', 'rgb(var(--primary))')
+            input.current.style.setProperty('--initialBar', '0')
+            input.current.style.setProperty('--initialLabelFontSize', '14px')
+            input.current.style.setProperty('--initialLabelTop', '10px')
+            input.current.style.setProperty('--labelColor', 'rgb(var(--titles))')
+        }
+    }
     return (
-    <div onMouseDown={(e)=>scrollToView(e, wrapper, isMobile)} className={`${styles.group} ${icon?styles.icon:null}`} style={marginBottom?{marginBottom: marginBottom+'px'}:null}>
+    <div ref={input} onMouseDown={(e)=>scrollToView(e, wrapper, isMobile)} className={`${styles.group} ${icon?styles.icon:null}`} style={marginBottom?{marginBottom: marginBottom+'px'}:null}>
         {icon?icon:null}
-        <input onTouchEnd={onTouchEnd?(e)=>onTouchEnd(e):null} autoComplete={autoComplete?autoComplete:null} id={id?id:null} onFocus={onFocus?(e)=>onFocus(e):null} autoFocus={autoFocus?autoFocus:null} onBlurCapture={onBlur?(e)=>onBlur(e):null} onChange={onChange?e=>onChange(e):null} defaultValue={value?value:null} type={type} required="required"/><span className={styles.highlight}></span><span className={styles.bar}></span>
-        <label>{name}</label>
+        <input onLoad={(e)=>onLoad(e)} onTouchEnd={onTouchEnd?(e)=>onTouchEnd(e):null} autoComplete={autoComplete?autoComplete:null} id={id?id:null} onFocus={onFocus?(e)=>onFocus(e):null} autoFocus={autoFocus?autoFocus:null} onBlurCapture={onBlur?(e)=>onBlur(e):null} onChange={onChange?e=>onChange(e):null} defaultValue={value?value:null} type={type} required="required"/><span className={styles.highlight}></span><span className={styles.bar}></span>
+        <label>{error?error:name}</label>
     </div>
     )
 }
--- src/screens/Subsidiary/Auth/index.jsx
+++ src/screens/Subsidiary/Auth/index.jsx
@@ -1,19 +1,64 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react'
+import { Link, Redirect } from 'react-router-dom'
 import company from '../../../company'
 import styles from './_auth.module.sass'
 import GoogleLogin from 'react-google-login'
 import {windowHeight} from '../Dashboard/variables/mobileHeights'
 import InputBox from './components/InputBox'
+import Backendless from 'backendless'
 
 const Auth = ({type}) => {
 
+    const inputText = useRef({
+        email: '',
+        password: '',
+        confirmpassword: ''
+    })
+
     useEffect(()=>{
         document.getElementsByTagName('html')[0].className = 'light'
+        let APP_ID = 'DB0DCF25-9468-8FAB-FFC0-F3BAE974FB00'
+        let API_KEY = '5CE4C303-32CB-498B-8645-DC70AD54F770'
+        Backendless.initApp(APP_ID, API_KEY)
     }, [])
+
+    useEffect(()=>{
+        if(document.getElementById('authEmail')){
+            inputText.current.email = document.getElementById('authEmail').innerText
+        }
+        if(document.getElementById('authPassword')){
+            inputText.current.password = document.getElementById('authPassword').innerText
+        }
+        if(document.getElementById('authConfirmPassword')){
+            inputText.current.confirmpassword = document.getElementById('authConfirmPassword').innerText
+        }
+    }, [inputText])
+
+    const [error, setError] = useState(false)
+
+    const onsubmit = () => {
+        if(type==='signup'){
+            if(inputText.current.password === inputText.current.confirmpassword){
+                if(error){
+                    setError(false)
+                }
+                let user = new Backendless.User()
+                user.email = inputText.current.email
+                user.password = inputText.current.password
+                Backendless.UserService.register( user ).then(()=>setRedirect(true)).catch((err)=>console.log(err))
+            }else{
+                setError('Password Mismatch')
+            }
+        }else{
+            Backendless.UserService.login(inputText.current.email, inputText.current.password, true).then(()=>setRedirect(true)).catch((err)=>setError(err.message))
+        }
+    }
+
+    const [redirect, setRedirect] = useState(false)
 
     return (
         <div className={styles.wrapper} style={{height: window.innerHeight+'px'}} id='authWrapper'>
+            {redirect?<Redirect to={`/${company.subsidiary}/dashboard/${company.journals}`} />:null}
             <div className={styles.auth} style={{height: windowHeight+'px'}}>
                 <div className={styles.form}>
                     <div className={styles.title}>
@@ -33,10 +78,10 @@ const Auth = ({type}) => {
                         <hr />
                     </div>
                     <div className={styles.input} id='authForm'>
-                        <InputBox marginBottom={28} wrapper='authWrapper' name="Email" type="text" />
-                        <InputBox marginBottom={28} wrapper='authWrapper' name="Password" type="password" />
-                        {type==='signup'?<InputBox marginBottom={40} wrapper='authWrapper' name="Confirm Password" type="password" />:null}
-                        <Link to={`/${company.subsidiary}/dashboard/${company.journals}`}>Continue</Link>
+                        <InputBox id='authEmail' onBlur={(e)=>inputText.current.email=e.target.value} onLoad={(e)=>inputText.current.email=e.target.value} onChange={(e)=>inputText.current.email=e.target.value} marginBottom={28} wrapper='authWrapper' name="Email" type="text" />
+                        <InputBox id='authPassword' onBlur={(e)=>inputText.current.email=e.target.value} onLoad={(e)=>inputText.current.password=e.target.value} error={error} onChange={(e)=>inputText.current.password=e.target.value} marginBottom={28} wrapper='authWrapper' name="Password" type="password" />
+                        {type==='signup'?<InputBox error={error} id='authConfirmPassword' onChange={(e)=>inputText.current.confirmpassword=e.target.value} marginBottom={40} wrapper='authWrapper' name="Confirm Password" type="password" />:null}
+                        <button onMouseDown={onsubmit}>Continue</button>
                     </div>
                     {type==='signup'?
                         <div className={styles.signin}>
