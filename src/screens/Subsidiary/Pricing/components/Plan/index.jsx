38cd38366d0d24fb2f1cc20d9a29bd4756ac36dd
Anmol Virk <theanmolvirk@gmail.com>
2022-02-05 01:31:09 +0530

    updated pricing page with features

--- src/screens/Subsidiary/Pricing/components/Plan/index.jsx
+++ src/screens/Subsidiary/Pricing/components/Plan/index.jsx
@@ -3,8 +3,15 @@ import styles from './_plan.module.sass'
 import {useHistory} from 'react-router-dom'
 import company from '../../../../../company'
 
-const Plan = ({title, subtitle, price, interval}) => {
-    let currentPrice = price-(price*0.5)
+const Plan = ({title, subtitle, price, interval, month, features}) => {
+    let orginalPrice = price
+    let halfPrice = orginalPrice/2
+    let monthOffPrice = false
+    if(!month){
+        orginalPrice = price*12
+        monthOffPrice = orginalPrice-price
+        halfPrice = monthOffPrice/2
+    }
     const history = useHistory()
     const submit = {
         starter: () => {
@@ -19,9 +26,75 @@ const Plan = ({title, subtitle, price, interval}) => {
     }
     return (
         <div className={`${styles.plan} ${title==='Pro'?styles.recommended:null}`}>
-            <h1>{title}</h1>
-            <h2>{subtitle}</h2>
-            <h3><span>{`$${currentPrice}`}</span> <span>per {interval}</span></h3>
+            <div className={styles.topcard}>
+                <h1>{title}</h1>
+                <h2>{subtitle} {title!=='Pro'?<span> + xxxxx xxxxxxx + xxxxxxxxxx xxxxxx xxxxxxxx</span>:null}</h2>
+                <div className={styles.price}>
+                    <span className={styles.mainprice}>{`$${halfPrice}`}</span> 
+                    <div className={styles.prevPrice}>
+                        {price!==0?
+                            <div className={styles.prices}>
+                                {monthOffPrice?<strike>{`$${monthOffPrice}`}</strike>:null}
+                                <strike>{`$${orginalPrice}`}</strike>
+                            </div>
+                        :null}
+                        <span>per {interval}</span>
+                    </div>
+                </div>
+                <div onMouseDown={submit[title.toLowerCase()]} className={styles.cta}>{price!==0?'Create Account':'Open Dashboard'}</div>
+            </div>
+            <div className={styles.features}>
+                <div className={`${styles.feature} ${styles.journals}`}>
+                    <div className={styles.title}>
+                        <img src='/logos/journals.png' alt={company.journals} />
+                    </div>
+                    <ul>
+                        {features.journals.map((item, i)=>{
+                            return <li key={i}>{item}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className={`${styles.feature} ${styles.schedule}`}>
+                    <div className={styles.title}>
+                        <img src='/logos/schedule.png' alt={company.schedule} />
+                    </div>
+                    <ul>
+                        {features.schedule.map((item, i)=>{
+                            return <li key={i}>{item}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className={`${styles.feature} ${styles.notes}`}>
+                    <div className={styles.title}>
+                        <img src='/logos/notes.png' alt={company.notes} />
+                    </div>
+                    <ul>
+                        {features.notes.map((item, i)=>{
+                            return <li key={i}>{item}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className={`${styles.feature} ${styles.finances}`}>
+                    <div className={styles.title}>
+                        <img src='/logos/finances.png' alt={company.finances} />
+                    </div>
+                    <ul>
+                        {features.finances.map((item, i)=>{
+                            return <li key={i}>{item}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className={`${styles.feature} ${styles.fitness}`}>
+                    <div className={styles.title}>
+                        <img src='/logos/fitness.png' alt={company.fitness} />
+                    </div>
+                    <ul>
+                        {features.fitness.map((item, i)=>{
+                            return <li key={i}>{item}</li>
+                        })}
+                    </ul>
+                </div>
+            </div>
             <div onMouseDown={submit[title.toLowerCase()]} className={styles.cta}>{price!==0?'Create Account':'Open Dashboard'}</div>
         </div>
     )
--- src/screens/Subsidiary/Pricing/index.jsx
+++ src/screens/Subsidiary/Pricing/index.jsx
@@ -13,22 +13,185 @@ const Pricing = () => {
     {
       title: 'Starter',
       subtitle: 'free forever',
-      price: 0
+      price: 0,
+      features: {
+        journals: [
+          'Up to 4 journals',
+          'Up to 10 entries per journal',
+          'Up to 6 notes per entry',
+          'Local browser storage',
+          'Limited customizable options',
+          'Single device'
+        ],
+        schedule: [
+          'Up to 6 habits',
+          'Up to 3 times limit for habits',
+          'Up to 3 projects',
+          'Up to 12 tasks',
+          'One layer of subtasks',
+          'Up to 12 events',
+          'Limited customizable options',
+          'Local browser storage',
+          'Single device'
+        ],
+        notes: [
+          'Up to 2 folders',
+          'Up to 6 mind maps per folder',
+          'Link upto 2 notes',
+          'No export options',
+          'Limited customizable options',
+          'Local browser storage',
+          'Single device'
+        ],
+        finances: [
+          'Manual budget tracking',
+          'Setting up to 5 financial goals',
+          'Up to 5 custom income and expense categories',
+          'Limited customizable options',
+          'Local browser storage',
+          'Single device'
+        ],
+        fitness: [
+          'Weight tracking',
+          'Gym logs',
+          'Up to 6 custom workout routines',
+          'Calorie tracking',
+          'Choose from an extensive list of meal plans',
+          'Up to 6 custom meals',
+          'Choose from an extensive list of recipies',
+          'Up to 6 custom recipies',
+          'Groccery list',
+          'Water tracking',
+          'Limited customizable options',
+          'Local browser storage',
+          'Single device'
+        ]
+      }
     },
     {
       title: 'Plus',
-      subtitle: 'free forever',
-      price: month?40:40*12-40
+      subtitle: 'unlocks everything',
+      price: 40,
+      features: {
+        journals: [
+          'Unlimited journals',
+          'Unlimited entries per journal',
+          'Unlimited notes per entry',
+          'Local browser storage',
+          'Unlocked customizable options',
+          'Single device'
+        ],
+        schedule: [
+          'Unlimited habits',
+          'Unlimited times limit for habits',
+          'Unlimited projects',
+          'Unlimited tasks',
+          'Unlimited layers of subtasks',
+          'Unlimited events',
+          'Unlocked customizable options',
+          'Local browser storage',
+          'Single device'
+        ],
+        notes: [
+          'Unlimited folders',
+          'Unlimited mind maps per folder',
+          'Link unlimited notes',
+          'No export options',
+          'Unlocked customizable options',
+          'Local browser storage',
+          'Single device'
+        ],
+        finances: [
+          'Manual budget tracking',
+          'Setting unlimited financial goals',
+          'Unlimited custom income and expense categories',
+          'Unlocked customizable options',
+          'Local browser storage',
+          'Single device'
+        ],
+        fitness: [
+          'Weight tracking',
+          'Gym logs',
+          'Unlimited custom workout routines',
+          'Calorie tracking',
+          'Choose from an extensive list of meal plans',
+          'Unlimited custom meal plans',
+          'Choose from an extensive list of recipies',
+          'Unlimited custom recipies',
+          'Groccery list',
+          'Water tracking',
+          'Limited customizable options',
+          'Local browser storage',
+          'Single device'
+        ]
+      }
     },
     {
       title: 'Pro',
-      subtitle: 'free forever',
-      price: month?50:50*12-50
+      subtitle: 'unlocks everything + cloud storage + additional online features',
+      price: 50,
+      features: {
+        journals: [
+          'Unlimited journals',
+          'Unlimited entries',
+          'Unlimited notes',
+          'Unlocked customizable options',
+          'Cloud storage',
+          'Multiple devices'
+        ],
+        schedule: [
+          'Unlimited habits',
+          'Unlimited times limit for habits',
+          'Unlimited projects',
+          'Unlimited tasks',
+          'Unlimited layers of subtasks',
+          'Unlimited events',
+          'Unlocked customizable options',
+          'Cloud storage',
+          'Multiple devices'
+        ],
+        notes: [
+          'Unlimited folders',
+          'Unlimited mind maps per folder',
+          'Link unlimited notes',
+          'Export as PDF',
+          'Unlocked customizable options',
+          'Cloud storage',
+          'Single device'
+        ],
+        finances: [
+          'Automatic budget tracking',
+          'Setting unlimited financial goals',
+          'Unlimited custom income and expense categories',
+          'Automatic seperation of bills and subscriptions',
+          'Investing in the stock market',
+          'Unlocked customizable options',
+          'Cloud storage',
+          'Multiple devices'
+        ],
+        fitness: [
+          'Weight tracking',
+          'Gym logs',
+          'Unlimited custom workout routines',
+          'Calorie tracking',
+          'Choose from an extensive list of meal plans',
+          'Unlimited custom meal plans',
+          'Choose from an extensive list of recipies',
+          'Unlimited custom recipies',
+          'Groccery list',
+          'Water tracking',
+          'Massive food database',
+          'barcode scanner',
+          'Unlocked customizable options',
+          'Cloud storage',
+          'Multiple devices'
+        ]
+      }
     }
   ]
 
   return (
-    <div className={styles.pricing}>
+    <div className={`${styles.pricing} light`} style={{height: window.innerHeight+'px'}}>
         <Header />
         <div className={styles.title}>
           <h1>Ready to start with <span>{company.subsidiary}</span>?</h1>
@@ -37,7 +200,7 @@ const Pricing = () => {
         <Toggle month={month} setMonth={setMonth} />
         <div className={styles.plans}>
           {plans.map((item, i)=>{
-            return <Plan {...item} interval={month?'month':'year'} key={i} />
+            return <Plan {...item} interval={month?'month':'year'} key={i} month={month} />
           })}
         </div>
     </div>
