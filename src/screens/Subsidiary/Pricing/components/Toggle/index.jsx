4a0c243319ccf21ba051b9d1b1217418f4401fa0
Anmol Virk <theanmolvirk@gmail.com>
2022-02-05 17:45:03 +0530

    updated mobile toggle pricing

--- src/screens/Subsidiary/Pricing/components/Toggle/index.jsx
+++ src/screens/Subsidiary/Pricing/components/Toggle/index.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from './_toggle.module.sass'
 
 const Toggle = ({month, setMonth}) => {
+    const isMobile = window.innerWidth < 1450
     return (
         <div className={styles.toggleWrapper}>
             <div className={styles.toggleContainer}>
                 <h3 style={{opacity: month?1:0.5}}>Monthly</h3>
                 <div className={styles.toggle} onMouseDown={()=>setMonth(!month)} style={{backgroundColor: !month?'rgb(var(--primary))':'rgb(var(--border))'}}>
-                    <hr style={{left: month?'0.5vh':'calc(100% - 3.5vh)'}} />
+                    <hr style={{left: month?isMobile?'5px':'0.5vh':isMobile?'calc(100% - 30px)':'calc(100% - 3.5vh)'}} />
                 </div>
                 <h3 style={{opacity: !month?1:0.5}}>Yearly</h3>
             </div>
