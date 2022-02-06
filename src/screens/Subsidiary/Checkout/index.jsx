46b5e0e4f778155bb49e6bdcee575bb7e404e810
Anmol Virk <theanmolvirk@gmail.com>
2022-02-06 23:07:38 +0530

    setting input type number

--- src/screens/Subsidiary/Checkout/index.jsx
+++ src/screens/Subsidiary/Checkout/index.jsx
@@ -5,6 +5,7 @@ import Header from '../LandingPage/components/Header'
 import styles from './_checkout.module.sass'
 import InputBox from '../Auth/components/InputBox'
 import company from '../../../company'
+import { useRef } from 'react'
 
 const Logo = () => {
   const [auth] = useRecoilState(authAtom)
@@ -19,19 +20,25 @@ const Logo = () => {
 
 const Checkout = () => {
   const [auth] = useRecoilState(authAtom)
+  const card = useRef({
+    num: '',
+    mm: '',
+    yy: '',
+    cvv: ''
+  })
   const Form = () => {
     return (
       <form className={styles.form}>
         <Logo />
         <div className={styles.cname}>
-          <InputBox type='text' name='Card number' autoComplete='cc-number' />
+          <InputBox type='number' name='Card number' autoComplete='cc-number' value={card.current.num} onChange={(e)=>card.current.num=e.target.value} />
         </div>
         <div className={styles.time}>
-          <InputBox type='text' name='MM' autoComplete='cc-exp-month' />
-          <InputBox type='text' name='YY' autoComplete='cc-exp-year' />
-          <InputBox type='text' name='CVC' autocomplete='cc-csc' />
+          <InputBox type='number' name='MM' autoComplete='cc-exp-month' onChange={(e)=>card.current.mm=e.target.value} />
+          <InputBox type='number' name='YY' autoComplete='cc-exp-year' onChange={(e)=>card.current.yy=e.target.value} />
+          <InputBox type='number' name='CVC' autocomplete='cc-csc' onChange={(e)=>card.current.cvv=e.target.value} />
         </div>
-        <button className={styles.cta}>Start {auth.plan.title} Plan</button>
+        <button className={styles.cta} onMouseDown={()=>alert(card.current.num)}>Start {auth.plan.title} Plan</button>
       </form>
     )
   }
