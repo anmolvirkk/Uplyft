24fef126721448e87851d96ccf5f4c1aad471c93
Anmol Virk <theanmolvirk@gmail.com>
2022-02-09 22:11:53 +0530

    mobile cta

--- src/screens/Subsidiary/LandingPage/components/Header/index.jsx
+++ src/screens/Subsidiary/LandingPage/components/Header/index.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import styles from './_header.module.sass'
 import company from '../../../../../company'
 import { Link } from 'react-router-dom'
+import {LogIn, UserPlus} from 'react-feather'
 
 const Header = ({type}) => {
+    const isMobile = window.innerWidth < 1450
     return (
         <div className={styles.header}>
             <div className={styles.navigation}>
@@ -18,10 +20,16 @@ const Header = ({type}) => {
                     <div className={styles.navItem}>Resources</div>
                 </div>
             </div>
+            {isMobile?
+            <div className={styles.mobileCta}>
+                <Link className={styles.loginBtn} to={`/${company.subsidiary}/login`}><LogIn /></Link>
+                {type==='pro'?null:<Link className={styles.ctaBtn} to={`/${company.subsidiary}/pricing`}><UserPlus /></Link>}
+            </div>
+            :
             <div className={styles.cta}>
                 <Link className={styles.loginBtn} to={`/${company.subsidiary}/login`}>Login</Link>
                 {type==='pro'?null:<Link className={styles.ctaBtn} to={`/${company.subsidiary}/pricing`}>Get Started</Link>}
-            </div>
+            </div>}
         </div>
     )
 }
