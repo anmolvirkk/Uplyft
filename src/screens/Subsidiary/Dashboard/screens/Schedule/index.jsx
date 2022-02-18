963ba7f2046f2db24c2a0732e526b5fb7f679b65
Anmol Virk <theanmolvirk@gmail.com>
2022-02-18 05:47:57 +0530

    schedule add dropdown style change

--- src/screens/Subsidiary/Dashboard/screens/Schedule/index.jsx
+++ src/screens/Subsidiary/Dashboard/screens/Schedule/index.jsx
@@ -189,99 +189,105 @@ const Schedule = React.memo(({updateBackendless, updateAtoms}) => {
 
             {isMobile?<MobileHeader updateBackendless={updateBackendless} updateAtoms={updateAtoms} />:null}
             
-            <div id='scheduleAddDropDownContainer' className={styles.scheduleAddDropDown} style={{transform: `translateY(${scheduleAddDropDown?0:150}%)`, top: document.getElementById('scheduleAddDropDownContainer')?`${window.innerHeight-80-document.getElementById('scheduleAddDropDownContainer').clientHeight}px`:'100vh'}}>
-                <OutsideClickHandler onOutsideClick={(e)=>closeAddMenu(e)}>
-                    <button onMouseDown={()=>setModalConfig({type: 'addhabit'})}><RefreshCw /><p>Add Habit</p></button>
-                    <button onMouseDown={()=>setModalConfig({type: 'addProject'})}><Folder /><p>Add Project</p></button>
-                    <button onMouseDown={()=>setModalConfig({type: 'addEvent'})}><Calendar /><p>Add Event</p></button>
-                </OutsideClickHandler>
-            </div>
-            <div id='scheduleSideMenu' className={styles.scheduleSideMenu} style={{height: `${sideMenuHeight}px`, maxHeight: `${sideMenuHeight}px`, transform: `translateX(${scheduleSideMenu?0:-100}%)`}}>
-                <OutsideClickHandler onOutsideClick={(e)=>closeSideMenu(e)}>
-                    <div className={`${styles.sideSectionSlot} ${styles.showCalendar}`} onMouseDown={showCalendar}>
-                        <div className={styles.slotContent}>
-                            <p>Show Calendar</p>
-                        </div>
-                    </div>   
-                    <ul className={styles.switchCategory}>
-                        <li onMouseDown={()=>setCurrentCategory('all')} className={currentCategory==='all'?styles.activeCategory:null}>All</li>
-                        <li onMouseDown={()=>setCurrentCategory('habits')} className={currentCategory==='habits'?styles.activeCategory:null}>Habits</li>
-                        <li onMouseDown={()=>setCurrentCategory('projects')} className={currentCategory==='projects'?styles.activeCategory:null}>Projects</li>
-                        <li onMouseDown={()=>setCurrentCategory('events')} className={currentCategory==='events'?styles.activeCategory:null}>Events</li>
-                    </ul>
-                    {currentCategory==='all'||currentCategory==='habits'?
-                        <div className={styles.sideMenuCategory}>
-                            <h3><p><RefreshCw />Habits</p><Plus onMouseDown={()=>setModalConfig({type: 'addhabit'})} /></h3>
-                            <div className={styles.options}>
-                                {habits.map((item)=>{
-                                    return (
-                                        <div onClick={(e)=>closeSidebarAfter({habit: item.id}, e.target, 'habits', item.name)} key={item.id} className={styles.sideSectionSlot} data-title={item.name}>
-                                            <div className={styles.slotContent}>
-                                                <div style={{backgroundColor: colors[item.color]}} className={styles.habitIcon}>
-                                                    {iconsSvg[item.icon]}
-                                                </div>
-                                                <p>{item.name}</p>
-                                            </div>
-                                            <div className={styles.habitOptions}>
-                                                <MoreMenu items={[{name: "edit", function: ()=>setModalConfig({type: 'edithabit', habit: item})}, {name: "delete", function: ()=>deleteHabit(item.id)}]} id={`scheduleSlotsMoreMenu${item.id}`} pos={{right: '-5vh', top: '3.5vh'}} />
-                                                <CheckBtn times={item.times} id={item.id} timesCompleted={item.timesCompleted} datesCompleted={item.datesCompleted} />
-                                            </div>
-                                        </div>   
-                                    )
-                                })}
+            {isMobile?
+                <div id='scheduleAddDropDownContainer' className={styles.scheduleAddDropDown} style={{transform: `translateY(${scheduleAddDropDown?0:150}%)`}}>
+                    <OutsideClickHandler onOutsideClick={(e)=>closeAddMenu(e)}>
+                        <button onMouseDown={()=>setModalConfig({type: 'addhabit'})}><RefreshCw /><p>Add Habit</p></button>
+                        <button onMouseDown={()=>setModalConfig({type: 'addProject'})}><Folder /><p>Add Project</p></button>
+                        <button onMouseDown={()=>setModalConfig({type: 'addEvent'})}><Calendar /><p>Add Event</p></button>
+                    </OutsideClickHandler>
+                </div>
+            :null}
+            
+            {isMobile?
+                <div id='scheduleSideMenu' className={styles.scheduleSideMenu} style={{height: `${sideMenuHeight}px`, maxHeight: `${sideMenuHeight}px`, transform: `translateX(${scheduleSideMenu?0:-100}%)`}}>
+                    <OutsideClickHandler onOutsideClick={(e)=>closeSideMenu(e)}>
+                        <div className={`${styles.sideSectionSlot} ${styles.showCalendar}`} onMouseDown={showCalendar}>
+                            <div className={styles.slotContent}>
+                                <p>Show Calendar</p>
                             </div>
-                        </div>
-                    :null}
-                    {currentCategory==='all'||currentCategory==='projects'?
-                        <div className={styles.sideMenuCategory}>
-                            <h3><p><Folder />Projects</p><Plus onMouseDown={()=>setModalConfig({type: 'addProject'})} /></h3>
-                            <div className={styles.options}>
-                                {projects.map((item)=>{
-                                    const completedTasks = projects.find(i=>i.id===item.id).tasks.filter(i=>i.completed===true)?projects.find(i=>i.id===item.id).tasks.filter(i=>i.completed===true).length:0
-                                    const totalTasks = projects.find(i=>i.id===item.id).tasks.length
-                                    return (
-                                        <div key={item.id} data-title={item.name} onClick={(e)=>closeSidebarAfter({project: item.id}, e.target, 'tasks', item.name)} className={styles.projectSideSectionSlot}>
-                                            <div className={styles.slotContent}>
-                                                <div className={styles.title}>
+                        </div>   
+                        <ul className={styles.switchCategory}>
+                            <li onMouseDown={()=>setCurrentCategory('all')} className={currentCategory==='all'?styles.activeCategory:null}>All</li>
+                            <li onMouseDown={()=>setCurrentCategory('habits')} className={currentCategory==='habits'?styles.activeCategory:null}>Habits</li>
+                            <li onMouseDown={()=>setCurrentCategory('projects')} className={currentCategory==='projects'?styles.activeCategory:null}>Projects</li>
+                            <li onMouseDown={()=>setCurrentCategory('events')} className={currentCategory==='events'?styles.activeCategory:null}>Events</li>
+                        </ul>
+                        {currentCategory==='all'||currentCategory==='habits'?
+                            <div className={styles.sideMenuCategory}>
+                                <h3><p><RefreshCw />Habits</p><Plus onMouseDown={()=>setModalConfig({type: 'addhabit'})} /></h3>
+                                <div className={styles.options}>
+                                    {habits.map((item)=>{
+                                        return (
+                                            <div onClick={(e)=>closeSidebarAfter({habit: item.id}, e.target, 'habits', item.name)} key={item.id} className={styles.sideSectionSlot} data-title={item.name}>
+                                                <div className={styles.slotContent}>
+                                                    <div style={{backgroundColor: colors[item.color]}} className={styles.habitIcon}>
+                                                        {iconsSvg[item.icon]}
+                                                    </div>
                                                     <p>{item.name}</p>
                                                 </div>
-                                                <div className={styles.title}>
-                                                    {item.id!=='all'&&item.id!=='today'?<div className={styles.projectMoreMenuWrapper}><MoreMenu items={[{name: "edit", function: ()=>setModalConfig({type: 'editProject', project: item})}, {name: "delete", function: ()=>deleteProject(item.id)}]} id={`scheduleSlotsMoreMenu${item.id}`} pos={{right: '-5vh', top: '3.5vh'}} /></div>:null} 
-                                                    <div className={styles.projectProgressNum} style={{marginLeft: '12px'}}>
-                                                        {completedTasks}/
-                                                        {totalTasks}
-                                                    </div>  
-                                                    <Plus className={styles.addTaskBtn} onMouseDown={()=>addTaskToProject({project: item.id})} />
+                                                <div className={styles.habitOptions}>
+                                                    <MoreMenu items={[{name: "edit", function: ()=>setModalConfig({type: 'edithabit', habit: item})}, {name: "delete", function: ()=>deleteHabit(item.id)}]} id={`scheduleSlotsMoreMenu${item.id}`} pos={{right: '-5vh', top: '3.5vh'}} />
+                                                    <CheckBtn times={item.times} id={item.id} timesCompleted={item.timesCompleted} datesCompleted={item.datesCompleted} />
+                                                </div>
+                                            </div>   
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        :null}
+                        {currentCategory==='all'||currentCategory==='projects'?
+                            <div className={styles.sideMenuCategory}>
+                                <h3><p><Folder />Projects</p><Plus onMouseDown={()=>setModalConfig({type: 'addProject'})} /></h3>
+                                <div className={styles.options}>
+                                    {projects.map((item)=>{
+                                        const completedTasks = projects.find(i=>i.id===item.id).tasks.filter(i=>i.completed===true)?projects.find(i=>i.id===item.id).tasks.filter(i=>i.completed===true).length:0
+                                        const totalTasks = projects.find(i=>i.id===item.id).tasks.length
+                                        return (
+                                            <div key={item.id} data-title={item.name} onClick={(e)=>closeSidebarAfter({project: item.id}, e.target, 'tasks', item.name)} className={styles.projectSideSectionSlot}>
+                                                <div className={styles.slotContent}>
+                                                    <div className={styles.title}>
+                                                        <p>{item.name}</p>
+                                                    </div>
+                                                    <div className={styles.title}>
+                                                        {item.id!=='all'&&item.id!=='today'?<div className={styles.projectMoreMenuWrapper}><MoreMenu items={[{name: "edit", function: ()=>setModalConfig({type: 'editProject', project: item})}, {name: "delete", function: ()=>deleteProject(item.id)}]} id={`scheduleSlotsMoreMenu${item.id}`} pos={{right: '-5vh', top: '3.5vh'}} /></div>:null} 
+                                                        <div className={styles.projectProgressNum} style={{marginLeft: '12px'}}>
+                                                            {completedTasks}/
+                                                            {totalTasks}
+                                                        </div>  
+                                                        <Plus className={styles.addTaskBtn} onMouseDown={()=>addTaskToProject({project: item.id})} />
+                                                    </div>
+                                                </div>
+                                                <div className={styles.progress}>
+                                                        <hr style={{width: `${completedTasks/totalTasks*100?completedTasks/totalTasks*100:0}%`}} />
                                                 </div>
                                             </div>
-                                            <div className={styles.progress}>
-                                                    <hr style={{width: `${completedTasks/totalTasks*100?completedTasks/totalTasks*100:0}%`}} />
-                                            </div>
-                                        </div>
-                                    )
-                                })}
+                                        )
+                                    })}
+                                </div>
                             </div>
-                        </div>
-                    :null}
-                    {currentCategory==='all'||currentCategory==='events'?
-                        <div className={styles.sideMenuCategory}>
-                            <h3><p><Calendar />Events</p><Plus onMouseDown={()=>setModalConfig({type: 'addEvent'})} /></h3>
-                            <div className={styles.options}>
-                                {events.map((item)=>{
-                                    return (
-                                        <div onClick={(e)=>closeSidebarAfter({event: item.id}, e.target, 'events', item.name)} key={item.id} className={`${styles.sideSectionSlot} ${styles.eventSlot}`} data-title={item.name}>
-                                            <div className={styles.eventSlot}>
-                                                <p>{item.name}</p>
-                                                <MoreMenu items={[{name: "edit", function: ()=>setModalConfig({type: 'editEvent', event: item})}, {name: "delete", function: ()=>deleteEvent(item.id)}]} id={`scheduleSlotsMoreMenu${item.id}`} pos={{right: '-1.5vh', top: '3.5vh'}} />
+                        :null}
+                        {currentCategory==='all'||currentCategory==='events'?
+                            <div className={styles.sideMenuCategory}>
+                                <h3><p><Calendar />Events</p><Plus onMouseDown={()=>setModalConfig({type: 'addEvent'})} /></h3>
+                                <div className={styles.options}>
+                                    {events.map((item)=>{
+                                        return (
+                                            <div onClick={(e)=>closeSidebarAfter({event: item.id}, e.target, 'events', item.name)} key={item.id} className={`${styles.sideSectionSlot} ${styles.eventSlot}`} data-title={item.name}>
+                                                <div className={styles.eventSlot}>
+                                                    <p>{item.name}</p>
+                                                    <MoreMenu items={[{name: "edit", function: ()=>setModalConfig({type: 'editEvent', event: item})}, {name: "delete", function: ()=>deleteEvent(item.id)}]} id={`scheduleSlotsMoreMenu${item.id}`} pos={{right: '-1.5vh', top: '3.5vh'}} />
+                                                </div>
                                             </div>
-                                        </div>
-                                    )
-                                })}
+                                        )
+                                    })}
+                                </div>
                             </div>
-                        </div>
-                    :null}
-                </OutsideClickHandler>
-            </div>
+                        :null}
+                    </OutsideClickHandler>
+                </div>
+            :null}
+
         </div>
     )
 })
