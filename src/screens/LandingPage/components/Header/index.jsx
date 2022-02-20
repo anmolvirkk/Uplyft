b0c111e908f8dc082203b4ae79ccee54c2d68571
Anmol Virk <theanmolvirk@gmail.com>
2022-02-21 03:18:41 +0530

    landing page header media query updated

--- src/screens/LandingPage/components/Header/index.jsx
+++ src/screens/LandingPage/components/Header/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import {LogIn, UserPlus} from 'react-feather'
 
 const Header = ({type}) => {
-    const isMobile = window.innerWidth < 1450
+    const isMobile = window.innerWidth < 640
     return (
         <div className={styles.header}>
             <div className={styles.navigation}>
