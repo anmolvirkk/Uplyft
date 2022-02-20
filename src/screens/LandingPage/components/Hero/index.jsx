16ea23e5a398bcf6954d939505e43e6e8daaed52
Anmol Virk <theanmolvirk@gmail.com>
2022-02-20 22:32:29 +0530

    updated landing page

--- src/screens/LandingPage/components/Hero/index.jsx
+++ src/screens/LandingPage/components/Hero/index.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <div className={styles.hero}>
             <div className={styles.content}>
-                <img className={styles.subsidiaryLogo} src='/logos/subsidiarys.png' alt={company.subsidiary} />
+                <img className={styles.subsidiaryLogo} src='/logos/subsidiary.png' alt={company.subsidiary} />
                 <h1>The ultimate toolkit to become the best version of yourself.</h1>
                 <p><span>{company.main} {company.subsidiary}</span> is a collection of multiple apps, each designed to help you conquer specific challenges in life.</p>
                 <CTA />
