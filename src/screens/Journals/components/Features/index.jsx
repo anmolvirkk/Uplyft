5bea2f547aebc385938bbecda369faabfb2a6676
Anmol Virk <theanmolvirk@gmail.com>
2022-02-25 04:19:04 +0530

    spirit landing page

--- src/screens/Journals/components/Features/index.jsx
+++ src/screens/Journals/components/Features/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import styles from './_features.module.sass'
+import Lottie from 'react-lottie-player'
+import meditate from '../../../../lottie/meditate.json'
+import webdesign from '../../../../lottie/webdesign.json'
+import design from '../../../../lottie/design.json'
 
 const Features = () => {
     const Feature = ({title, text}) => {
@@ -12,20 +16,31 @@ const Features = () => {
             </div>
         )
     }
+    const SubHero = ({title, lottie, items}) => {
+        return (
+            <div className={styles.subhero}>
+                <div className={styles.content}>
+                    <h2>{title}</h2>
+                    {items.map((item)=>{
+                        return (
+                            <Feature {...item} />
+                        )
+                    })}
+                </div>
+                <Lottie
+                    play
+                    loop
+                    animationData={lottie}
+                    style={{ minWidth: 500 }}
+                />
+            </div>
+        )
+    }
     return (
         <div className={styles.features}>
-            <h2>Journaling is a powerful tool for mindfulness</h2>
-            <p>Spirit will help you to be more mindful and grateful by journaling about your day, thoughts, feelings or experiences.</p>
-            <div className={styles.list}>
-                <Feature title="100% Private, Secure and Encrypted" text="Journal with no fear of judgement or being judged from others reading it because your journal is secure and encrypted such that only you have access to it." />
-                <Feature title="Stress-free journaling experience" text="Free from ads, distractions or pressure of being perfect, Spirit will help you be as calm as possible by making space for your feelings in the moment." />
-                <Feature title="Increase Positive Energy" text="Spirit is a safe space to explore, grow and find peace in the comfort of your own thoughts." />
-                <Feature title="Build Healthy Thinking" text="Journaling is proven to help reduce stress, anxiety and depression." />
-                <Feature title="Unparalleled Organization" text="You'll have everything organized in one place so that when you're ready to write, there's no need to hunt around for what comes next on your list of things to do or say." />
-                <Feature title="Daily Mindfulness Practice" text="Every day you'll receive an email or notification to take some time for yourself so that you can reflect on the things that are going well in your life today." />
-                <Feature title="Beautiful design" text="We have created an elegant interface with the best UX in the industry so it's easy to use even if you are not tech savvy!" />
-                <Feature title="Customizable" text="Choose from a plethora of icons and colors to create a completely personalized experience that is perfect for you!" />
-            </div>
+            <SubHero title="Journaling is a powerful tool for mindfulness" lottie={meditate} items={[{ title:"Stress-free journaling experience", text:"Free from ads, distractions or pressure of being perfect, Spirit will help you be as calm as possible by making space for your feelings in the moment."}, {title:"Build Healthy Thinking", text:"Journaling is proven to help reduce stress, anxiety and depression."}, {title:"Increase Positive Energy", text:"Spirit is a safe space to explore, grow and find peace in the comfort of your own thoughts."}]} />
+            <SubHero title="All you need is your thoughts, and we'll do the rest" lottie={webdesign} items={[{title:"100% Private, Secure and Encrypted", text:"Journal with no fear of judgement or being judged from others reading it because your journal is secure and encrypted such that only you have access to it." }, {title:"Unparalleled Organization", text:"You'll have everything organized in one place so that when you're ready to write, there's no need to hunt around for what comes next on your list of things to do or say."}, {title:"Daily Mindfulness Practice", text:"Every day you'll receive an email or notification to take some time for yourself so that you can reflect on the things that are going well in your life today."}]} />
+            <SubHero title="Create your own personalized sanctuary" lottie={design} items={[{title:"Beautiful design", text:"We have created an elegant interface with the best UX in the industry so it's easy to use even if you are not tech savvy!"}, { title:"Customizable", text:"Choose from a plethora of icons and colors to create a completely personalized experience that is perfect for you!"}]} />
         </div>
     )
 }
