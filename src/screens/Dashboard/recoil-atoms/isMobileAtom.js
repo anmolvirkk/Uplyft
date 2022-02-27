c4e3bf78f45cc1600296928eccd7e5d890e0df0a
Anmol Virk <theanmolvirk@gmail.com>
2022-02-28 04:27:20 +0530

    update ismobile atom

--- src/screens/Dashboard/recoil-atoms/isMobileAtom.js
+++ src/screens/Dashboard/recoil-atoms/isMobileAtom.js
@@ -1,12 +1,8 @@
 import {atom} from 'recoil'
-import {recoilPersist} from 'recoil-persist'
-
-const {persistAtom} = recoilPersist()
 
 const isMobileAtom = atom({
     key: 'isMobile',
-    default: window.innerWidth < 1450,
-    effects_UNSTABLE: [persistAtom]
+    default: window.innerWidth < 639,
 })
 
 export default isMobileAtom
