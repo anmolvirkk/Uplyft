fe1b744cd69e4db26564456d02e0f7d4f66f63d2
Anmol Virk <theanmolvirk@gmail.com>
2022-03-03 07:21:00 +0530

    updated hero section

--- src/screens/LandingPage/components/Hero/index.jsx
+++ src/screens/LandingPage/components/Hero/index.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <div className={styles.content}>
                 <img className={styles.subsidiaryLogo} src='/logos/main.png' alt={company.subsidiary} />
                 <h1>The ultimate toolkit to become the best version of yourself.</h1>
-                <p><span>{company.main} {company.subsidiary}</span> is a collection of multiple apps, each designed to help you conquer specific challenges in life.</p>
+                <p><span>{company.subsidiary}</span> is a collection of multiple apps, each designed to help you conquer specific challenges in life.</p>
                 <CTA />
             </div>
             <img loading='lazy' decoding='async' className={styles.heroImg} src = '/decor/hero2.jpg' alt={company.subsidiary} />
